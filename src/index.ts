f2e8d088fd55ef8cb007937b2a1c573bd168a991
tony-tsx <tony.js@zoho.eu>
2020-02-18 12:18:41 -0300

    Initial Types of cookie cli

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+declare namespace CookieCli {
+  export namespace Command {
+    export namespace Detached {
+      export type Function<Args = Object> = ( argv: string[], args: Args ) => FunctionReturn
+    }
+    export type Function<Args = Object> = ( argv: Array<string>, ctx: Config, args: Args ) => FunctionReturn
+    export type FunctionReturn = Promise<void> | void
+    export namespace Option {
+      export type Value = string | boolean | number
+    }
+    export interface Option<T = ( ctx: Config ) => Option.Value> {
+      name: string
+      description?: string
+      parse?: ( val: string ) => any
+      default?: Option.Value | T
+    }
+  }
+  export interface Command<IsDetached extends boolean = false> {
+    name: string
+    description?: string
+    options?: Command.Option<
+      IsDetached extends true ? () => Command.Option.Value : ( ctx: Config ) => Command.Option.Value
+    >[]
+    detached?: IsDetached
+    examples?: ( { desc: string, cmd: string } )[]
+    pkg?: { name: string, version: string }
+    func: IsDetached extends true ? Command.Detached.Function : Command.Function
+  }
+  export interface Config {
+    commands?: Command[]
+  }
+}
+
+export = CookieCli
---
+++ types/index.d.ts
@@ -0,0 +1,38 @@
+declare namespace CookieCli {
+    namespace Command {
+        namespace Detached {
+            type Function<Args = Object> = (argv: string[], args: Args) => FunctionReturn;
+        }
+        type Function<Args = Object> = (argv: Array<string>, ctx: Config, args: Args) => FunctionReturn;
+        type FunctionReturn = Promise<void> | void;
+        namespace Option {
+            type Value = string | boolean | number;
+        }
+        interface Option<T = (ctx: Config) => Option.Value> {
+            name: string;
+            description?: string;
+            parse?: (val: string) => any;
+            default?: Option.Value | T;
+        }
+    }
+    interface Command<IsDetached extends boolean = false> {
+        name: string;
+        description?: string;
+        options?: Command.Option<IsDetached extends true ? () => Command.Option.Value : (ctx: Config) => Command.Option.Value>[];
+        detached?: IsDetached;
+        examples?: ({
+            desc: string;
+            cmd: string;
+        })[];
+        pkg?: {
+            name: string;
+            version: string;
+        };
+        func: IsDetached extends true ? Command.Detached.Function : Command.Function;
+    }
+    interface Config {
+        commands?: Command[];
+    }
+}
+export = CookieCli;
+//# sourceMappingURL=index.d.ts.map
